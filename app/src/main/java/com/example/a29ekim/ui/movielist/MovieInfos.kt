ea746fe04e08f7dd2d20c7d3a0bf094a55d4a54a
burcaksahn <burcak.sahn@hotmail.com>
2021-11-04 20:48:34 +0300

    Add api service

---
+++ app/src/main/java/com/example/a29ekim/ui/movielist/MovieInfos.kt
@@ -0,0 +1,24 @@
+package com.example.a29ekim.ui.movielist
+
+import com.google.gson.annotations.Expose
+
+import com.google.gson.annotations.SerializedName
+
+
+class MovieInfos{
+    @SerializedName("page")
+    @Expose
+    var page: Int? = null
+
+    @SerializedName("results")
+    @Expose
+    var results: List<Result>? = null
+
+    @SerializedName("total_pages")
+    @Expose
+    var totalPages: Int? = null
+
+    @SerializedName("total_results")
+    @Expose
+    var totalResults: Int? = null
+}
--- app/src/main/java/com/example/a29ekim/ui/movielist/MovieListFragment.kt
+++ app/src/main/java/com/example/a29ekim/ui/movielist/MovieListFragment.kt
@@ -1,11 +1,16 @@
 package com.example.a29ekim.ui.movielist
 
 import android.os.Bundle
+import android.util.Log
+import retrofit2.Call
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.a29ekim.R
+import com.example.a29ekim.utils.GetService
+import retrofit2.Callback
+import retrofit2.Response
 
 class MovieListFragment : Fragment() {
 
@@ -17,5 +22,23 @@ class MovieListFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_movie_list, container, false)
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+    private lateinit var api:GetService
+    private fun getData(){
+        api=GetService.getInstance()
+        var list:List<Result?>?=null
+        val call: Call<MovieInfos> = api.getAllMovieList("22574df9e1fe27a06f9bce371fb6aa2a", 1)
+        call.enqueue(object :Callback<MovieInfos>{
+            override fun onResponse(call: Call<MovieInfos>, response: Response<MovieInfos>) {
+                Log.d("TAG", "onResponse: "+response.body()?.results?.get(0)?.title)
+            }
+
+            override fun onFailure(call: Call<MovieInfos>, t: Throwable) {
+            }
+        })
     }
 }
